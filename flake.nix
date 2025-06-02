f38ae17741584aa4d704b66a91cfd0c747d6a47d
Robin Appelman <robin@icewind.nl>
2025-06-02 21:04:28 +0200

    flake update

--- flake.nix
+++ flake.nix
@@ -1,16 +1,17 @@
 {
   inputs = {
-    nixpkgs.url = "nixpkgs/nixos-24.11";
+    nixpkgs.url = "nixpkgs/nixos-25.05";
     flakelight = {
       url = "github:nix-community/flakelight";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     mill-scale = {
-      url = "github:icewind1991/mill-scale";
+      url = "git+https://codeberg.org/icewind/mill-scale";
       inputs.flakelight.follows = "flakelight";
     };
   };
-  outputs = { mill-scale, ... }: mill-scale ./. {
-    extraPaths = [ ./data ];
-  };
+  outputs = {mill-scale, ...}:
+    mill-scale ./. {
+      extraPaths = [./data];
+    };
 }
