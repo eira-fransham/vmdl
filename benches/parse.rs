ba17203e59872ba13d12fb60eff0318b7a5d1db0
Robin Appelman <robin@icewind.nl>
2025-07-05 15:18:40 +0200

    cargo updates

--- benches/parse.rs
+++ benches/parse.rs
@@ -1,5 +1,6 @@
-use criterion::{black_box, criterion_group, criterion_main, Criterion};
+use criterion::{criterion_group, criterion_main, Criterion};
 use std::fs::read;
+use std::hint::black_box;
 use vmdl::mdl::Mdl;
 use vmdl::vtx::Vtx;
 use vmdl::vvd::Vvd;
